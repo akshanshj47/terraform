1269442e839b90fdee3ba8df7905abe556bfd08d
root <root@ip-172-31-42-44.ap-south-1.compute.internal>
2022-09-16 13:16:36 +0000

    Myfourst Commit

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIA5SJ3FQCMU6PTSFVM"
-  secret_key = "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"
+  access_key = "AKIA5SJ3FQCM3YJB4Z2X"
+  secret_key = "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"
   region  = "ap-south-1"
 }
